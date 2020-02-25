fc8f80917175adc0ab9a3e51aea80653741724e4
Julien Savoie <julien@savoie.dev>
2020-02-25 17:38:44 -0500

    front-page.php

---
+++ front-page.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the front-page
+ *
+ * This is the template that displays the front-page by default.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package pied-cormorant
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		
+        
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
