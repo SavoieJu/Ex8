6be6a7655f3d1a389d919f2a0a3a59d7b50998aa
Julien Savoie <julien@savoie.dev>
2020-02-27 11:41:56 -0500

    Style page front-page.php

--- front-page.php
+++ front-page.php
@@ -11,11 +11,31 @@
 
 get_header();
 ?>
-
+	<img class="hero-img-pc" src="https://windows10spotlight.com/wp-content/uploads/2018/12/dde71c707fa28234470f4a203e598728.jpg" alt="Pied Cormorant">
+	<h1 class="pc-site-title"><?php bloginfo( 'name' ); ?></h1>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		
+		<?php 
+		// the query
+		$the_query = new WP_Query( array(
+			'category_name' => 'nouvelle',
+			'posts_per_page' => 3,
+		)); 
+		?>
+
+		<?php if ( $the_query->have_posts() ) : ?>
+		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+		<a href="<?php the_permalink(); ?>"><h1><?php the_title(); ?></h1></a>
+		<p><?php the_excerpt(); ?></p>
+
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+
+		<?php else : ?>
+		<span><?php __('Pas de nouvelle'); ?></span>
+		<?php endif; ?>
         
 		</main><!-- #main -->
 	</div><!-- #primary -->
